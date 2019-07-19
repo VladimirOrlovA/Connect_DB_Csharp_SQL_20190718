04671dfcc72a2cee714d512b9e68292a6f361d4f
Vladimir Orlov <vladimir.orlov.a@mail.ru>
2019-07-19 09:26:14 +0600

    Add project files.

---
+++ Connect_DB_Csharp_SQL_20190718/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect_DB_Csharp_SQL_20190718
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
